40a0c4a5bbb9fd7ee6cdd3d2867a24cff2935439
dptrias <danielpereztrias@gmail.com>
2024-10-01 12:35:53 +0200

    Bearing designer excentricity and elongation

--- BearingDesigner.m
+++ BearingDesigner.m
@@ -7,7 +7,7 @@ classdef BearingDesigner < handle
 
     properties
         parameters = bearing_parameters();
-        data_path = get_data_path();
+        data = get_data();
     end
 
     methods 
@@ -17,10 +17,11 @@ classdef BearingDesigner < handle
         end
 
         function load(BD)
-            if false
-                load_excentricity(BD);
-            elseif true
-                load_elongation(BD);
+            tol = 1e-8;
+            if "excentricity" == BD.parameters.last_updated
+                load_excentricity(BD, tol);
+            elseif "elongation" == BD.parameters.last_updated
+                load_elongation(BD, tol);
             end
         end
     end
@@ -28,23 +29,46 @@ classdef BearingDesigner < handle
 end
 
 function P = bearing_parameters()
-    P.elongation = 1;
-    P.excentricity = 0.05;
-    P.load = 3;
+    P.elongation = 0.1; %0.0199497487437186;
+    P.excentricity = 0.001994974874372;
+    P.load = 1.662403806113810e-06;
     P.last_updated = 'excentricity';
 end
 
-function path = get_data_path()
+function data = get_data()
     currentFile = mfilename('fullpath');
     [currentPath,~,~] = fileparts(currentFile);
     path = fullfile(currentPath, 'data');
     addpath(path);
+    data = load("e0001-0999_L001-100.mat");
 end
 
-function load_excentricity(BD)
-    
+function load_excentricity(BD, tol)
+    exc_index = (abs(BD.data.epsilon(:) - BD.parameters.excentricity) < tol);
+    if any(exc_index)
+        f_index = (abs(BD.data.f(:, exc_index) - BD.parameters.load) < tol);
+        if any(f_index)
+            disp(f_index);
+            BD.parameters.elongation = BD.data.Lambda(f_index);
+        else
+            disp('I did not find any value')
+        end
+    else
+        disp('I did not find any value')
+    end
 end
 
-function load_elongation(BD)
-    
+function load_elongation(BD, tol)
+    elo_index = (abs(BD.data.Lambda(:) - BD.parameters.elongation) < tol);
+    if any(elo_index)
+        f_index = (abs(BD.data.f(elo_index, :) - BD.parameters.load) < tol);
+        if any(f_index)
+            disp(f_index')
+            BD.parameters.excentricity = BD.data.epsilon(f_index');
+        else
+            disp('I did not find any value')
+        end
+    else
+        disp('I did not find any value')
+    end
 end
